8de968a4ae2a5c66d2dbdd17142f9f1066567b3a
JasonWang <1103870790@qq.com>
2019-01-09 15:40:22 +0800

    init repo

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <component :is="tagName" :class="classes">
+        <span ref="slot"><slot></slot></span>
+    </component>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: [],
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+    classes () {
+      return ['fluent-button']
+    },
+    tagName () {
+      return 'button'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+    @import '../styles/index.less';
+</style>
